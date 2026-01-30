cdf698f3b63c20795dec0c4c21d2db5f1d096829
Alex <56205947+Alexjhz07@users.noreply.github.com>
2026-01-30 12:55:27 -0800

    Clang Format (Squashed Commit) (#14)
    clang format workflow
    
    auto format (no sort includes)
    
    manual format ms5611

--- apps/rockets/cloudburst/src/pyro/pyro_thread.h
+++ apps/rockets/cloudburst/src/pyro/pyro_thread.h
@@ -5,19 +5,19 @@
 #include <stdbool.h>
 
 // Pyro status bit definitions
-#define PYRO_STATUS_DROGUE_FIRED    (1 << 0)
-#define PYRO_STATUS_MAIN_FIRED      (1 << 1)
-#define PYRO_STATUS_DROGUE_FAIL     (1 << 2)
-#define PYRO_STATUS_MAIN_FAIL       (1 << 3)
-#define PYRO_STATUS_DROGUE_CONT_OK  (1 << 4)
-#define PYRO_STATUS_MAIN_CONT_OK    (1 << 5)
+#define PYRO_STATUS_DROGUE_FIRED (1 << 0)
+#define PYRO_STATUS_MAIN_FIRED (1 << 1)
+#define PYRO_STATUS_DROGUE_FAIL (1 << 2)
+#define PYRO_STATUS_MAIN_FAIL (1 << 3)
+#define PYRO_STATUS_DROGUE_CONT_OK (1 << 4)
+#define PYRO_STATUS_MAIN_CONT_OK (1 << 5)
 #define PYRO_STATUS_DROGUE_FIRE_ACK (1 << 6)
-#define PYRO_STATUS_MAIN_FIRE_ACK   (1 << 7)
+#define PYRO_STATUS_MAIN_FIRE_ACK (1 << 7)
 
 // Pyro command definitions
-#define PYRO_CMD_FIRE_DROGUE    0x01
-#define PYRO_CMD_FIRE_MAIN      0x02
-#define PYRO_CMD_STATUS_REQ     0x55
+#define PYRO_CMD_FIRE_DROGUE 0x01
+#define PYRO_CMD_FIRE_MAIN 0x02
+#define PYRO_CMD_STATUS_REQ 0x55
 
 // Pyro status structure
 typedef struct {
